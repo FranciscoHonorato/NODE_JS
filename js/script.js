74fcfa02ecf86f6af311f218e359d26aa11686df
Your Name <you@example.com>
2024-09-12 22:51:39 -0300

    atualizacao menu

--- js/script.js
+++ js/script.js
@@ -2,18 +2,31 @@ let burguer = document.querySelector('.burguer')
 let itemMenu = document.querySelector('.itens-menu')
 let itemList = document.querySelectorAll('.item')
 
+let bgLight = document.querySelectorAll('.bg-light')
+let light = document.querySelectorAll('.light')
+
+
 function clickMenu() {
-    
-        if (itemMenu.style.display == 'none') {
-            itemMenu.style.display = "flex"
-        } else{
-            itemMenu.style.display = "none"
-        }
+
+    if (itemMenu.style.display == 'none') {
+        itemMenu.style.display = "flex"
+    } else {
+        itemMenu.style.display = "none"
+    }
 }
 
-if(burguer.style.display === 'flex'){
+
+if (burguer.style.display === 'flex') {
     for (let item of itemList) {
         item.addEventListener('click', clickMenu)
-    
     }
+}
+
+
+function mudarTema() {
+  for (let bg of bgLight) {
+   bg.classList.toggle('bg-dark')
+  }
+  
+    light[0].classList.toggle('dark')
 }
